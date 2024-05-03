a49bcb00da9358b260dfa6f463870567132696d5
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2024-05-03 22:24:34 +0300

    Update DatabaseUser.java

--- DatabaseUser.java
+++ DatabaseUser.java
@@ -2,16 +2,16 @@ import java.util.Arrays;
 import java.util.Vector;
 
 public class DatabaseUser {
-    static final Vector<String> courses  = new Vector<>(Arrays.asList ("java Programming", "Web Development", "Data Science", "Machine Learning"));
-    final Vector<String> name ;
-    final Vector<String>  password;
-    final Vector<Integer> type ;
+   static Vector<String> courses  = null;
+     static Vector<String> name =null;
+     static Vector<String>  password=null;
+     static Vector<Integer> type=null ;
 
     DatabaseUser() {
         name = new Vector<>(Arrays.asList ("sname1", "sname2", "sname3", "iname1", "iname2", "iname3"));
         password= new Vector<>(Arrays.asList ( "spass1", "spass2", "spass3", "ipass1", "ipass2", "ipass3"));
         type = new Vector<>(Arrays.asList (1,1,1,2,2,2));
-
+        courses=new Vector<>(Arrays.asList ("java Programming", "Web Development", "Data Science", "Machine Learning"));
     }
 
     public void enterDataUser(String name, String password,int type){
