9c7c10af5295d9b1223720c1d36be956e33db76d
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2024-05-03 22:11:39 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,13 @@ import java.util.Vector;
 public class Main {
 
     public static void main(String[] args) {
-        Vector <User> user =new Vector<>();
-        User.initializationDateUser(user);
+        User user=new User();
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        user.name = input.nextLine();
+        System.out.print("Enter your password: ");
+        user.password = input.nextLine();
+
         authentication x=new authentication();
         x.startAuthentication(user);
     }
