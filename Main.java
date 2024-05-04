f1de12e3e0b1eadeec4dd10c665b00f291782c29
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2024-05-04 08:43:49 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,24 +3,36 @@ import java.util.Vector;
 public class Main {
 
     public static void main(String[] args) {
-    User user=new User();
+        //Initializing  User
+        User user=new User();
         Scanner input=new Scanner(System.in);
         System.out.print("Enter your name: ");
         user.name = input.nextLine();
         System.out.print("Enter your password: ");
         user.password = input.nextLine();
 
+        //Add Courses to this user
+        user.courses.addCourses("HtmlAndCss",user.name);
+        Vector<String> newcourses=new Vector<>(Arrays.asList ("java Programming", "Web Development", "Data Science", "Machine Learning"));
+        user.courses.addCourses(newcourses,user.name);
+
         authentication x=new authentication();
         x.startAuthentication(user);
 
+        //Add new user in Database
         DatabaseUser data = new DatabaseUser();
         data.enterDataUser("sname4","spass4",1);
-        data.enterCourse("HtmlAndCss");
 
+        //Initializing  New User
         System.out.print("Enter your name: ");
         user.name = input.nextLine();
         System.out.print("Enter your password: ");
         user.password = input.nextLine();
+
+        //Add Courses to new user
+        user.courses.addCourses("Python",user.name);
+        user.courses.addCourses("Java",user.name);
+
         x.startAuthentication(user);
 
     }
