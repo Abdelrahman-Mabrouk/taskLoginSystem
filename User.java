b9bbfecb66b73af3b9c00cbd85af68760b047a73
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2024-05-04 08:44:13 +0300

    Update User.java

--- User.java
+++ User.java
@@ -3,15 +3,17 @@ public class User {
     String name;
     String password;
     int type;
-
+    Course courses;
     User(){
         this.name="defaultName";
         this.password="defaultPassword";
         this.type=1;
+        this.courses=new Course();
     }
     User(String name,String password,int type){
         this.name=name;
         this.password=password;
         this.type=type;
+        this.courses=new Course();
     }
 }
